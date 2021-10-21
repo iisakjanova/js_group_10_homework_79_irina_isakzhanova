8748d74b90f39550955acdbb43d3df25c8c052e3
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-10-21 20:43:54 +0600

    Implemented CRUD for locations

--- api/app/locations.js
+++ api/app/locations.js
@@ -1,4 +1,71 @@
 const express = require('express');
 const router = express.Router();
 
+const mysqlDb = require('../mysqlDb');
+
+router.get('/', async (req, res) => {
+    const data = await mysqlDb.getLocations();
+    res.send(data);
+});
+
+router.get('/:id', async (req, res) => {
+    const data = await mysqlDb.getLocationById(req.params.id);
+
+    if(!data) {
+        return res.status(404).send({error: 'Location is not found'});
+    }
+
+    res.send(data);
+});
+
+router.post('/', async (req, res) => {
+    if (!req.body.name) {
+        res.status(400).send({"error": "Incorrect data"});
+        return;
+    }
+
+    const location = {
+        name: req.body.name,
+        description: req.body.description,
+    };
+
+    const newData = await mysqlDb.addLocation(location);
+
+    res.send({
+        ...location,
+        id: newData.insertId,
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+    const data = await mysqlDb.deleteLocation(req.params.id);
+
+    if (data.affectedRows === 0) {
+        return res.status(404).send({error: 'Location is not found'});
+    }
+
+    res.send({message: `id = ${req.params.id} deleted successfully!`});
+});
+
+router.put('/:id', async (req, res) => {
+    if (!req.body.name) {
+        res.status(400).send({error: "Incorrect data"});
+        return;
+    }
+
+    const location = {
+        name: req.body.name,
+        description: req.body.description,
+    };
+
+    const data = await mysqlDb.editLocation({...location}, req.params.id);
+
+    if (data.affectedRows === 0) {
+        return res.status(404).send({error: 'Location is not found'});
+    }
+
+    res.send({message: `id = ${req.params.id} updated successfully!`});
+});
+
+
 module.exports = router;
--- api/mysqlDb.js
+++ api/mysqlDb.js
@@ -3,6 +3,9 @@ const config = require('./config');
 
 let connection = null;
 
+const categories = 'categories';
+const locations = 'locations';
+
 module.exports = {
     connect: async () => {
         connection = await mysql.createConnection(config.databaseOptions);
@@ -13,7 +16,7 @@ module.exports = {
     async getCategories() {
         const [data] = await this.getConnection().query(
             'SELECT ??, ?? FROM ??',
-            ['id', 'name', 'categories']
+            ['id', 'name', categories]
         );
 
         return data;
@@ -21,7 +24,7 @@ module.exports = {
     async getCategoryById(id) {
         const [data] = await this.getConnection().query(
             `SELECT * FROM ?? WHERE id = ?`,
-            ['categories', id]
+            [categories, id]
         );
 
         return data[0];
@@ -29,7 +32,7 @@ module.exports = {
     async addCategory(item) {
         const data = await this.getConnection().query(
             'INSERT INTO ?? (name, description) VALUES (?, ?)',
-            ['categories', item.name, item.description]
+            [categories, item.name, item.description]
         );
 
         return data[0];
@@ -37,7 +40,7 @@ module.exports = {
     async deleteCategory(id) {
         const [data] = await this.getConnection().query(
             'DELETE FROM ?? WHERE id = ?',
-            ['categories', id]
+            [categories, id]
         );
 
         return data;
@@ -45,7 +48,47 @@ module.exports = {
     async editCategory(item, id) {
         const [data] = await this.getConnection().query(
             'UPDATE ?? SET ? WHERE id = ?',
-            ['categories', item, id]
+            [categories, item, id]
+        );
+        return data;
+    },
+
+    async getLocations() {
+        const [data] = await this.getConnection().query(
+            'SELECT ??, ?? FROM ??',
+            ['id', 'name', locations]
+        );
+
+        return data;
+    },
+    async getLocationById(id) {
+        const [data] = await this.getConnection().query(
+            `SELECT * FROM ?? WHERE id = ?`,
+            [locations, id]
+        );
+
+        return data[0];
+    },
+    async addLocation(item) {
+        const data = await this.getConnection().query(
+            'INSERT INTO ?? (name, description) VALUES (?, ?)',
+            [locations, item.name, item.description]
+        );
+
+        return data[0];
+    },
+    async deleteLocation(id) {
+        const [data] = await this.getConnection().query(
+            'DELETE FROM ?? WHERE id = ?',
+            [locations, id]
+        );
+
+        return data;
+    },
+    async editLocation(item, id) {
+        const [data] = await this.getConnection().query(
+            'UPDATE ?? SET ? WHERE id = ?',
+            [locations, item, id]
         );
         return data;
     },
